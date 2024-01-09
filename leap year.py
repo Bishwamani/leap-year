5cbb491661da7c2aa4fdf81bc13a7faa78f4500b
Bishwamani Pokhrel <153804149+Bishwamani@users.noreply.github.com>
2024-01-10 03:29:30 +0800

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,13 @@
+input_year = 2600
+year = int(input_year)
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")   
+    else:
+        print("Leap year")  
+else:
+    print("Not leap year")    
